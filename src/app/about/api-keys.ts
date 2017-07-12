a3dc29d0ec399dca37dc200d2bac5d656379da8e
corey redding <coreysnightout@gmail.com>
2017-07-11 23:51:31 -0700

    find errors in the api-keys.ts file and module component

---
+++ src/app/about/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyB1e1ZoujX3vbax3JeDnAFeSElptL4LJbA",
+    authDomain: "my-awesome-project-a5442.firebaseapp.com",
+    databaseURL: "https://my-awesome-project-a5442.firebaseio.com",
+    storageBucket: "my-awesome-project-a5442.appspot.com",
+    messagingSenderId: "825123420727"
+};
